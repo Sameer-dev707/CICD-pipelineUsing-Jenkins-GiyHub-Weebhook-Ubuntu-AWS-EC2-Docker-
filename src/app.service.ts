74a2a05f9bcc12fcd6a20fa13a12604aadc84589
Sameer-dev707 <msameerhassan11@gmail.com>
2026-09-06 16:54:08 -0700

    solveing4

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World my name is muhammad sameer hassan!';
+    return 'Hello World my name is Muhammad Sameer hassan!';
   }
 }
